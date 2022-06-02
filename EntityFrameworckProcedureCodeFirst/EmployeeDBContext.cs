6d31caa4c64fc90d8e24d43a11ea6e6d98153375
ciorca <ciorca.gaby@gmail.com>
2022-06-02 11:39:35 +0300

    Changing the procedure name and parameters using code first

--- EntityFrameworckProcedureCodeFirst/EmployeeDBContext.cs
+++ EntityFrameworckProcedureCodeFirst/EmployeeDBContext.cs
@@ -11,7 +11,21 @@ namespace EntityFrameworckProcedureCodeFirst
         public DbSet<Employee> Employees { get; set; }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Employee>().MapToStoredProcedures();
+            modelBuilder.Entity<Employee>().MapToStoredProcedures
+                (p => p.Insert(i => i.HasName("InsertEmployee")
+                               .Parameter(n => n.Name, "EmployeeName")
+                               .Parameter(n => n.Gender, "EmployeeGender")
+                               .Parameter(n => n.Salary, "EmployeeSalary"))
+                       .Update(u => u.HasName("UnpdateEmployee")
+                                .Parameter(n => n.ID, "EmployeeID")
+                               .Parameter(n => n.Name, "EmployeeName")
+                              .Parameter(n => n.Gender, "EmployeeGender")
+                              .Parameter(n => n.Salary, "EmployeeSalary"))
+                       .Delete(d => d.HasName("DeleteEmployee")
+                                .Parameter(n => n.ID, "EmployeeID"))
+
+                );
+
             base.OnModelCreating(modelBuilder);
         }
     }
